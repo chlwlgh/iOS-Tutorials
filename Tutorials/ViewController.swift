04810ba16d3cace3ebfe2fff28366891cd4bfe65
kor45cw <kor45cw@gmail.com>
2019-08-10 16:27:50 +0900

    UI: main view designed

--- Tutorials/ViewController.swift
+++ Tutorials/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
+extension ViewController {
+    @IBAction private func created(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction private func GET1(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction private func GET2(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction private func POST(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction private func PUT(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction private func PATCH(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction private func DELETE(_ sender: UIButton) {
+        
+    }
+}
