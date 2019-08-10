18fa8dfade7e58fa39b0e104becbd8cf9c914bde
kor45cw <kor45cw@gmail.com>
2019-08-10 16:16:47 +0900

    Initial commit
    add Podfile

---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '11.0'
+inhibit_all_warnings!
+use_frameworks!
+
+
+target 'Tutorials' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  
+
+  # Pods for Tutorials
+  pod 'Alamofire', '~> 5.0.0-beta.7'
+
+end
---
+++ Tutorials/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tutorials
+//
+//  Created by kor45cw on 10/08/2019.
+//  Copyright © 2019 kor45cw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
